7bc85904409e8dbd777ac18b898384e6cdf03ea7
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-11 18:20:32 +0100

    added render url to requestMethods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:7000/api/"
+// const BASE_URL = "http://localhost:7000/api/"
+const BASE_URL = "https://shoppy-api-m4i9.onrender.com/api/"
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjYwNDdjZGNmZDZlODZlOGY3NWYxMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDYxMDg3NSwiZXhwIjoxNjgxMjE1Njc1fQ.B2lY2lCI7odmhgh_rOKVxDOpU2G3NVIxrNWqiOCprm8"
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
