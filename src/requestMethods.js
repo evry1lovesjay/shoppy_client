cf286e77ec8ef027f51743109ad040452a8845d6
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-06-01 13:01:38 +0200

    added prod & dev

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,20 +1,26 @@
 import axios from "axios"
 
 // const BASE_URL = "http://localhost:7000/api/"
-const BASE_URL = "https://shoppy-api-m4i9.onrender.com/api/"
+// const BASE_URL = "https://shoppy-api-m4i9.onrender.com/api/"
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjYwNDdjZGNmZDZlODZlOGY3NWYxMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDYxMDg3NSwiZXhwIjoxNjgxMjE1Njc1fQ.B2lY2lCI7odmhgh_rOKVxDOpU2G3NVIxrNWqiOCprm8"
+
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 
+const API_URL = process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_URL_PROD
+    : process.env.REACT_APP_API_URL_DEV;
+    
+
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+    baseURL: `${API_URL}/api/`,
 })
 
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
+    baseURL: `${API_URL}/api/`,
     headers:{token: `Bearer ${TOKEN}`},
 })
